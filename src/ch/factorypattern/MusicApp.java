1d1b6d6053f41f6bf44857fa67ac001ffa24323e
Michael Wettstein <chischte@gmx.ch>
2020-01-22 19:07:11 +0100

    Change method names

--- src/ch/factorypattern/MusicApp.java
+++ src/ch/factorypattern/MusicApp.java
@@ -9,13 +9,17 @@ package ch.factorypattern;
 public class MusicApp {
 
 	public static void main(final String[] args) {
+		// create factory:
 		MusicFactory musicFactory = MusicFactory.getInstance();
-		AbstractSong aClassicSong = musicFactory.getSong("Classic", "Symphony No.9", "Bach");
-		AbstractSong aPopSong = musicFactory.getSong("Pop", "Toxic", "Britney Spears");
-		AbstractSong anotherPopSong = musicFactory.getSong("Pop", "Another Day", "Phil Collins");
+		// create songs:
+		AbstractSong aClassicSong = musicFactory.createSong("Classic", "Symphony No.9", "Bach");
+		AbstractSong aPopSong = musicFactory.createSong("Pop", "Toxic", "Britney Spears");
+		AbstractSong anotherPopSong = musicFactory.createSong("Pop", "Another Day", "Phil Collins");
+		// get info:
 		System.out.println(aPopSong.getInterpret());
 		System.out.println(anotherPopSong.getTitle());
 		System.out.println(aClassicSong.getTitle());
+		// set/get chart placement:
 		aPopSong.setChartPlacement(22);
 		System.out.println(aPopSong.getChartPlacement());
 	}

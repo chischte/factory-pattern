adcb5773d73534ef367138dcdb0eff2c846e942c
Michael Wettstein <chischte@gmx.ch>
2020-01-22 15:30:10 +0100

    Start implementation of testing

---
+++ src/ch/testing/PopSongTest.java
@@ -0,0 +1,28 @@
+// check out this tutorial:
+// https://www.tutorialspoint.com/junit/junit_environment_setup.htm
+
+/**
+ * Test class of MusicApp
+ * 
+ * @author Michael Wettstein
+ */
+/*
+package ch.testing;
+
+import static jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import ch.factorypattern.PopSong;
+
+class PopSongTest {
+	private PopSong fileType;
+	
+	@Test
+	void testPlaySong() {
+        fileType = new PopSong("Roxette", "Look Sharp");
+		   
+		assertEquals(fileType.playSong(),"lalalalala ...this is a pop song called ");
+	}
+	  }
+	  */

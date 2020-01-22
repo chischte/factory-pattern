635aeae8c9a0b33bff86612b73bcc9ace56c6448
Michael Wettstein <chischte@gmx.ch>
2020-01-22 10:59:19 +0100

    Add singleton to factory

--- src/ch/factorypattern/ComputerApp.java
+++ src/ch/factorypattern/ComputerApp.java
@@ -1,14 +1,12 @@
 
 package ch.factorypattern;
 
-import ch.factorypattern.ComputerFactory;
-import ch.factorypattern.AbstractComputer;
-
 public class ComputerApp {
 
 	public static void main(final String[] args) {
-		AbstractComputer pc = ComputerFactory.getComputer("pc", "2 GB", "500 GB", "2.4 GHz");
-		AbstractComputer server = ComputerFactory.getComputer("server", "16 GB", "1 TB", "2.9 GHz");
+		SingletonComputerFactory computerFactory = SingletonComputerFactory.getInstance();
+		AbstractComputer pc = computerFactory.getComputer("pc", "2 GB", "500 GB", "2.4 GHz");
+		AbstractComputer server = computerFactory.getComputer("server", "16 GB", "1 TB", "2.9 GHz");
 		System.out.println("Factory PC Config::" + pc);
 		System.out.println("Factory Server Config::" + server);
 	}
--- src/ch/factorypattern/SingletonComputerFactory.java
+++ src/ch/factorypattern/SingletonComputerFactory.java
@@ -4,19 +4,29 @@ import ch.factorypattern.AbstractComputer;
 import ch.factorypattern.PCComputer;
 import ch.factorypattern.ServerComputer;
 
-public class ComputerFactory {
+public class SingletonComputerFactory {
 
 	/**
 	 * The singleton instance
 	 */
 
-
-
-	 
+	private static SingletonComputerFactory theInstance = null;
+	
+	//private SingletonComputerFactory(){		
+	//}
+	
+	public static SingletonComputerFactory getInstance(){		
+		if(theInstance == null){
+			theInstance = new SingletonComputerFactory();
+		}
+		
+		return theInstance;
+	}
+		 
 	/**
 	 * Creates an object
 	 */	
-	public static AbstractComputer getComputer(String type, String ram, String hdd,
+	public AbstractComputer getComputer(String type, String ram, String hdd,
 			 String cpu) {
 		if ("PC".equalsIgnoreCase(type))
 			return new PCComputer(ram, hdd, cpu);
